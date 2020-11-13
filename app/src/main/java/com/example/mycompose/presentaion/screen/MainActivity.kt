b6fedae21340b548337623d11b558925203a3208
Kim Minkyu <minkyu9403@gmail.com>
2020-11-13 16:08:49 +0900

    바텀 네비게이션 구현, 쉴드 클래스 사용

--- app/src/main/java/com/example/mycompose/presentaion/screen/MainActivity.kt
+++ app/src/main/java/com/example/mycompose/presentaion/screen/MainActivity.kt
@@ -2,46 +2,108 @@ package com.example.mycompose.presentaion.screen
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Icon
 import androidx.compose.foundation.Text
-import androidx.compose.foundation.layout.Column
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Scaffold
-import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.*
 import androidx.ui.tooling.preview.Preview
+import com.example.mycompose.presentaion.screen.common.Screen
 import com.example.mycompose.presentaion.ui.MyComposeTheme
-import com.example.mycompose.presentaion.ui.purple200
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            main()
+            MyApp()
+        }
+    }
+}
+@Composable
+fun MyApp() {
+    val navController = rememberNavController()
+    MyComposeTheme {
+        Scaffold(
+            bottomBar = { bottomNavigation(navController) }
+        ) {
+            NavHost(navController, startDestination = Screen.Home.route) {
+                composable(Screen.Home.route) { Home(navController) }
+                composable(Screen.Review.route) { Review(navController) }
+                composable(Screen.Pocket.route) { Pocket(navController) }
+                composable(Screen.Report.route) { Report(navController) }
+                composable(Screen.More.route) { More(navController) }
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-private fun main() {
-    MyComposeTheme {
-        // A surface container using the 'background' color from the theme
-        Scaffold(
-                topBar = {
-                    TopAppBar(title = { Text("TopAppBar") }, backgroundColor = purple200)
-                },
-                bodyContent = {
-                    Column() {
-                        Greeting("Android")
-                        Greeting()
-                    }
-                }
-        )
-    }
+fun Home(navController: NavController) {
+    Text(text = "Home")
 }
 
 @Composable
-fun Greeting(name: String = "Greeting") {
-    Text(text = "Hello $name!", fontSize = TextUnit.Companion.Sp(10))
+fun Review(navController: NavController) {
+    Text(text = "Home")
+}
+
+@Composable
+fun Pocket(navController: NavController) {
+    Text(text = "Home")
+}
+
+@Composable
+fun Report(navController: NavController) {
+    Text(text = "Home")
+}
+
+@Composable
+fun More(navController: NavController) {
+    Text(text = "Home")
+}
+
+@Composable
+fun bottomNavigation(navController: NavHostController = rememberNavController()) {
+    val items = listOf(
+        Screen.Home,
+        Screen.Review,
+        Screen.Pocket,
+        Screen.Report,
+        Screen.More,
+    )
+    BottomNavigation {
+        val navBackStackEntry = navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry.value?.arguments?.getString(KEY_ROUTE)
+
+        for (screen in items) {
+            BottomNavigationItem(
+                icon = { Icon(screen.icon) },
+                label = { Text(stringResource(screen.resourceId)) },
+                selected = currentRoute == screen.route,
+                onClick = {
+                    // This is the equivalent to popUpTo the start destination
+                    navController.popBackStack(navController.graph.startDestination, false)
+
+                    // This if check gives us a "singleTop" behavior where we do not create a
+                    // second instance of the composable if we are already on that destination
+                    if (currentRoute != screen.route) {
+                        navController.navigate(screen.route)
+                    }
+                }
+            )
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    Text(text = "122")
 }
--- app/src/main/java/com/example/mycompose/presentaion/screen/common/Screen.kt
+++ app/src/main/java/com/example/mycompose/presentaion/screen/common/Screen.kt
@@ -1,12 +1,18 @@
 package com.example.mycompose.presentaion.screen.common
 
 import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.ui.graphics.vector.VectorAsset
 import com.example.mycompose.R
 
-sealed class Screen(val route: String, @StringRes val resourceId: Int) {
-    object Home : Screen("home", R.string.profile)
-    object Review : Screen("review", R.string.friends_list)
-    object Pocket : Screen("pocket", R.string.friends_list)
-    object Report : Screen("report", R.string.friends_list)
-    object More : Screen("more", R.string.friends_list)
+sealed class Screen(val route: String, @StringRes val resourceId: Int, val icon: VectorAsset) {
+    object Home : Screen("home", R.string.main_tab_home, Icons.Filled.Home)
+    object Review : Screen("review", R.string.main_tab_review, Icons.Filled.Favorite)
+    object Pocket : Screen("pocket", R.string.main_tab_pocket, Icons.Filled.Edit)
+    object Report : Screen("report", R.string.main_tab_report, Icons.Filled.Favorite)
+    object More : Screen("more", R.string.main_tab_more, Icons.Filled.Settings)
 }
