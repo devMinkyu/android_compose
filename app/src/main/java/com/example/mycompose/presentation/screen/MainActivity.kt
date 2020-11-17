41492fd28ed05796505fd8e7274dde29deebb207
Kim Minkyu <minkyu9403@gmail.com>
2020-11-17 09:36:56 +0900

    화면쪽 메인 모듈화 진행

--- app/src/main/java/com/example/mycompose/presentation/screen/MainActivity.kt
+++ app/src/main/java/com/example/mycompose/presentation/screen/MainActivity.kt
@@ -2,15 +2,10 @@ package com.example.mycompose.presentation.screen
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.res.stringResource
-import androidx.navigation.NavController
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.*
 import androidx.ui.tooling.preview.Preview
-import com.example.mycompose.presentation.screen.common.Screen
+import com.example.mycompose.presentation.screen.main.MainScreen
 import com.example.mycompose.presentation.ui.MyComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,89 +14,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text ="Home")
-        }
-    }
-}
-@Composable
-fun MyApp() {
-    val navController = rememberNavController()
-    MyComposeTheme {
-        Scaffold(
-            bottomBar = { bottomNavigation(navController) }
-        ) {
-            NavHost(navController, startDestination = Screen.Home.route) {
-                composable(Screen.Home.route) { Home(navController) }
-                composable(Screen.Review.route) { Review(navController) }
-                composable(Screen.Pocket.route) { Pocket(navController) }
-                composable(Screen.Report.route) { Report(navController) }
-                composable(Screen.More.route) { More(navController) }
+            MyApp {
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Home(navController: NavController) {
-    Text(text = "Home")
-}
-
-@Composable
-fun Review(navController: NavController) {
-    Text(text = "Review")
-}
-
-@Composable
-fun Pocket(navController: NavController) {
-    Text(text = "Pocket")
-}
-
-@Composable
-fun Report(navController: NavController) {
-    Text(text = "Report")
-}
-
-@Composable
-fun More(navController: NavController) {
-    Text(text = "Home")
-}
-
-@Composable
-fun bottomNavigation(navController: NavHostController = rememberNavController()) {
-    val items = listOf(
-        Screen.Home,
-        Screen.Review,
-        Screen.Pocket,
-        Screen.Report,
-        Screen.More,
-    )
-    BottomNavigation {
-        val navBackStackEntry = navController.currentBackStackEntryAsState()
-        val currentRoute = navBackStackEntry.value?.arguments?.getString(KEY_ROUTE)
-
-        for (screen in items) {
-            BottomNavigationItem(
-                icon = { Icon(screen.icon) },
-                label = { Text(stringResource(screen.resourceId)) },
-                selected = currentRoute == screen.route,
-                onClick = {
-                    // This is the equivalent to popUpTo the start destination
-                    navController.popBackStack(navController.graph.startDestination, false)
-
-                    // This if check gives us a "singleTop" behavior where we do not create a
-                    // second instance of the composable if we are already on that destination
-                    if (currentRoute != screen.route) {
-                        navController.navigate(screen.route)
-                    }
-                }
-            )
-        }
+private fun MyApp(content: @Composable () -> Unit) {
+    MyComposeTheme {
+        content()
     }
 }
-
 
 @Preview
 @Composable
 fun DefaultPreview() {
-    Text(text = "122")
+    MyApp {
+        MainScreen()
+    }
 }
--- app/src/main/java/com/example/mycompose/presentation/ui/Color.kt
+++ app/src/main/java/com/example/mycompose/presentation/ui/Color.kt
@@ -6,3 +6,5 @@ val purple200 = Color(0xFFBB86FC)
 val purple500 = Color(0xFF6200EE)
 val purple700 = Color(0xFF3700B3)
 val teal200 = Color(0xFF03DAC5)
+
+val white500 = Color(0xfff8f8f8)
